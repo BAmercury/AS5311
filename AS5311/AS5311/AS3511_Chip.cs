fc3734c860ae9a598c41de6fb6d4bc3b9ca97832
BrianAmin <tuf49787@temple.edu>
2018-02-01 17:19:19 -0500

    Wrote up a C# version of the driver. Haven't tested it

---
+++ AS5311/AS5311/AS3511_Chip.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArduinoDriver;
+using ArduinoDriver.SerialProtocol;
+using ArduinoUploader.Hardware;
+using ArduinoUploader;
+using System.Threading;
+
+namespace AS5311
+{
+    public class AS3511_Chip
+    {
+
+        private ArduinoDriver.ArduinoDriver _driver;
+        private byte _DataPin;
+        private byte _ChipSelectPin;
+        private byte _IndexPin;
+        private byte _ClockPin;
+
+
+        public AS3511_Chip(ArduinoDriver.ArduinoDriver driver, int DataPin, int ChipSelectPin, int IndexPin, int ClockPin)
+        {
+
+            _driver = driver;
+            _DataPin = (byte)DataPin;
+            _ChipSelectPin = (byte)ChipSelectPin;
+            _IndexPin = (byte)IndexPin;
+            _ClockPin = (byte)ClockPin;
+
+
+        }
+
+        public UInt32 encoder_position()
+        {
+            UInt32 value;
+            value = encoder_value();
+            return ((value * 2) / 4096);
+
+
+        }
+
+        public UInt32 encoder_value()
+        {
+            return (read_chip() >> 6);
+
+        }
+
+
+        public UInt32 read_chip()
+        {
+            UInt32 raw_value = 0;
+            UInt16 inputstream = 0;
+            UInt16 c;
+            using (_driver)
+            {
+                _driver.Send(new DigitalWriteRequest(_ChipSelectPin, DigitalValue.High));
+                _driver.Send(new DigitalWriteRequest(_ClockPin, DigitalValue.High));
+                Thread.Sleep(100); //Will later replace with something more efficient
+                _driver.Send(new DigitalWriteRequest(_ChipSelectPin, DigitalValue.Low));
+                Thread.Sleep(10);
+                _driver.Send(new DigitalWriteRequest(_ClockPin, DigitalValue.Low));
+                Thread.Sleep(10);
+                for (c = 0; c < 18; c++)
+                {
+                    _driver.Send(new DigitalWriteRequest(_ClockPin, DigitalValue.High));
+                    Thread.Sleep(10);
+                    inputstream = Convert.ToUInt16(_driver.Send(new DigitalReadRequest(_DataPin)));
+                    raw_value = ((raw_value << 1) + inputstream);
+                    _driver.Send(new DigitalWriteRequest(_ClockPin, DigitalValue.Low));
+                    Thread.Sleep(10);
+                }
+            }
+
+            return raw_value;
+
+        }
+    }
+}
---
+++ AS5311/AS5311/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArduinoDriver;
+using ArduinoDriver.SerialProtocol;
+using ArduinoUploader.Hardware;
+using ArduinoUploader;
+
+namespace AS5311
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const ArduinoModel Arduino = ArduinoModel.UnoR3;
+
+            const int DataPin = 4;
+            const int ClockPin = 3;
+            const int ChipSelectPin = 2;
+            const int IndexPin = 1;
+
+
+            using (var driver = new ArduinoDriver.ArduinoDriver(Arduino, true))
+            {
+
+                driver.Send(new PinModeRequest(DataPin, PinMode.Input));
+                driver.Send(new PinModeRequest(ClockPin, PinMode.Output));
+                driver.Send(new PinModeRequest(ChipSelectPin, PinMode.Output));
+                driver.Send(new PinModeRequest(IndexPin, PinMode.Input));
+
+
+                while (true)
+                {
+                    AS3511_Chip chip = new AS3511_Chip(driver,DataPin,ChipSelectPin,IndexPin,ClockPin));
+
+                    long value;
+                    value = chip.encoder_value();
+                    Console.Write("Measured: ");
+                    Console.WriteLine(value);
+
+                    long pos;
+                    pos = chip.encoder_position
+                    
+
+                }
+
+            }
+
+
+   
+        }
+    }
+}
+
